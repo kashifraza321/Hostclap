54c6a25bffed50dd692b5ea3ab74ee169ab2b96c
tazzfatma <tazz4jmi@gmail.com>
2025-03-16 01:42:56 +0530

    add text animation

--- src/app/components/ca/ca.component.ts
+++ src/app/components/ca/ca.component.ts
@@ -7,9 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './ca.component.html',
   styleUrl: './ca.component.css',
 })
-export class CaComponent {
+export class CaComponent
+{
+  
   private carousel: any;
-
+   text = 'your Local Business';
+  displayText = '';
+  i = 0;
+  isDeleting = false;
+  targetIndex = 20; 
+constructor() {
+    this.type();
+  }
   ngAfterViewInit() {
     this.carousel = document.querySelector('#cardCarousel');
   }
@@ -20,5 +29,28 @@ export class CaComponent {
 
   startSlider() {
     this.carousel?.carousel?.('cycle');
+  }
+  type() {
+    if (!this.isDeleting) {
+      this.displayText = this.text.slice(0, this.i);
+      this.i++;
+
+      if (this.i === this.targetIndex) {
+        this.isDeleting = true;
+        setTimeout(() => this.type(), 1000); // Pause before deleting
+        return;
+      }
+    } else {
+      this.displayText = this.text.slice(0, this.i);
+      this.i--;
+
+      if (this.i === 0) {
+        this.isDeleting = false;
+        setTimeout(() => this.type(), 500); // Pause before typing again
+        return;
+      }
+    }
+
+    setTimeout(() => this.type(), this.isDeleting ? 100 : 150);
   }
 }
